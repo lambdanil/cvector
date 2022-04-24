dc7fa34d887110b9abea9a70b498f5bb6bd98e16
Jan Novotný <59669705+CuBeRJAN@users.noreply.github.com>
2022-04-24 20:30:17 +0200

    Add files via upload

---
+++ example2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "vector.h"
+
+int main() {
+    printf("Enter some numbers:\n");
+    vector numbers;
+    vector_init(&numbers, 0);
+    int current;
+    int added;
+    float average;
+    char c;
+    while (1) {
+        if (scanf("%d",&current) == 1) {
+            vector_push(&numbers, current);
+            added = 0;
+            for (int i = 0; i < (int)numbers.size; i++) added+=(vector_at(&numbers,i));
+            average = (float) added / (int)numbers.size;
+            printf("Average of entered numbers is %0.3f\n",average);
+        }
+        else {
+            printf("Average of numbers:");
+            for (int i = 0; i < (int)numbers.size-1; i++) printf(" %d,", vector_at(&numbers,i));
+            printf(" %d", vector_at(&numbers, (int)numbers.size-1));
+            average = (float) added / (int)numbers.size;
+            printf(" is %0.3f \n",average);
+            break;
+        }
+    }
+    vector_free(&numbers);
+
+}
