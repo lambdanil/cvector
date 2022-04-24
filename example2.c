8424de741362ab4be9db16efc9a23fc663e312f7
Jan Novotný <59669705+CuBeRJAN@users.noreply.github.com>
2022-04-24 20:31:54 +0200

    Update example2.c

--- example2.c
+++ example2.c
@@ -14,14 +14,14 @@ int main() {
         if (scanf("%d",&current) == 1) {
             vector_push(&numbers, current);
             added = 0;
-            for (int i = 0; i < (int)numbers.size; i++) added+=(vector_at(&numbers,i));
+            for (int i = 0; i < (int)numbers.size; i++) added+=(vector.at[i]);
             average = (float) added / (int)numbers.size;
             printf("Average of entered numbers is %0.3f\n",average);
         }
         else {
             printf("Average of numbers:");
-            for (int i = 0; i < (int)numbers.size-1; i++) printf(" %d,", vector_at(&numbers,i));
-            printf(" %d", vector_at(&numbers, (int)numbers.size-1));
+            for (int i = 0; i < (int)numbers.size-1; i++) printf(" %d,", vector.at[i]);
+            printf(" %d", numbers.at[(int)numbers.size-1]);
             average = (float) added / (int)numbers.size;
             printf(" is %0.3f \n",average);
             break;
